7c74fd6b6fd548f8bccfc14185a90187d1fe14d3
张敏慧 <zhangminhui@catsti.com>
2020-01-07 10:34:11 +0800

    更新样式、

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     proxyTable: {
       "/catsic": {
         // target: "http://124.192.215.4:8083",
-        target: "http://172.16.169.62:8888",
+        target: "http://172.16.169.78:8888",
         // target: "http://127.0.0.1:8888",
         changeOrigin: true,
       }
