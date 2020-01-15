1ab74f2f7b0a7f6e1f472db2ad71f995353e849d
张敏慧 <zhangminhui@catsti.com>
2020-01-15 15:59:27 +0800

    询问笔录

--- src/page/caseHandle/case/modle/QAModle.vue
+++ src/page/caseHandle/case/modle/QAModle.vue
@@ -30,9 +30,9 @@
             </el-select>
           </el-col>
         </el-row>
-       
 
-      </el-form-item> {{addBannerForm.domains}}
+        {{addBannerForm.domains}}
+      </el-form-item> {{addBannerForm.domains[0]}}{{addBannerForm.domains[1]}}
       <!-- <el-form-item label="问1：">
         <el-select filterable allow-create v-model="addBannerForm.domains[0].question">
           <el-option label="不认识执法人员，不需要回避" value="no"></el-option>
@@ -42,20 +42,20 @@
       <el-form-item label="答1：">
         <el-input type="textarea" :autosize="{ minRows: 2,}" placeholder="请输入内容" v-model="addBannerForm.domains[0].answer"></el-input>
       </el-form-item> -->
-      
-      <span v-for="(domain, index) in addBannerForm.domains"  :key="domain.key">
-        <el-form-item :label="'问' + (index +1)" :prop="'domains.' + (index +1)+ '.question'">
+
+      <span v-for="(domain, index) in addBannerForm.domains" :key="domain.key">
+        <el-form-item :label="'问' + (index +1)" >
           <!-- <el-select v-model="domain.question" filterable allow-create>
             <el-option label="不认识执法人员，不需要回避" question="no"></el-option>
             <el-option label="认识执法人员，需要回避" question="yes"></el-option>
           </el-select> -->
-          <el-input type="textarea" :autosize="{ minRows: 2,}" v-model="domain.question"></el-input>
+          <el-input type="textarea" :autosize="{ minRows: 2,}" maxlength="40" v-model="domain.question"></el-input>
         </el-form-item>
-        <el-form-item :label="'答' + (index +1)" :prop="'domains.' + (index +1) + '.answer'" :rules="{
+        <el-form-item :label="'答' + (index +1)"  :rules="{
       required: true, message: '不能为空', trigger: 'blur'}">
           <el-row :gutter="10">
             <el-col :span="21">
-              <el-input type="textarea" :autosize="{ minRows: 2,}" v-model="domain.answer"></el-input>
+              <el-input type="textarea" :autosize="{ minRows: 2,}"  maxlength="80" v-model="domain.answer"></el-input>
             </el-col>
             <el-col :span="1">
               <el-button @click.prevent="removeDomain(domain)" size="medium">删除</el-button>
@@ -66,8 +66,8 @@
     </el-form>
     <span slot="footer" class="dialog-footer">
       <el-button @click="addDomain">新增</el-button>
-      <!-- <el-button type="primary" @click="submitForm('addBannerForm')">提交</el-button> -->
-      <el-button type="primary" @click="overFloeEdit()">确定</el-button>
+      <el-button type="primary" @click="submitForm('addBannerForm')">提交</el-button>
+      <el-button type="primary" @click="overFloeEdit('addBannerForm')">确定</el-button>
     </span>
   </el-dialog>
 
@@ -103,23 +103,26 @@ export default {
       //this.errorOrganName = false;
     },
     //确定
-    overFloeEdit() {
+    overFloeEdit(formName) {
       //将当前内容传到父组件
-      console.log('模板',this.addBannerForm.domains)
+      let dataList = this.addBannerForm.domains;
+      console.log('模板', this.addBannerForm.domains)
+      console.log('dataList', dataList)
       this.$emit("QAModleInfo", this.addBannerForm.domains);
       this.visible = false;
       //   this.reload();
     },
-    // submitForm(formName) {
-    //   this.$refs[formName].validate((valid) => {
-    //     if (valid) {
-    //       alert('submit!');
-    //     } else {
-    //       console.log('error submit!!');
-    //       return false;
-    //     }
-    //   });
-    // },
+    submitForm(formName) {
+      console.log('模板', this.addBannerForm.domains)
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
     resetForm(formName) {
       this.$refs[formName].resetFields();
     },
