5e8bf116b5c8f6ce92794b94a8dffcebf2af19da
yuyaqiang@catsti.com <yuyaqiang@catstic.com>
2020-02-17 23:34:47 +0800

    人员信息

--- src/components/personComponents/adjustingPosts.vue
+++ src/components/personComponents/adjustingPosts.vue
@@ -1,5 +1,19 @@
 <template>
-
+  <el-table
+    :data="tableData"
+    border
+    style="width: 100%">
+    <el-table-column prop="date" label="转出单位"></el-table-column>
+    <el-table-column prop="name" label="转出时间"></el-table-column>
+    <el-table-column prop="address" label="转出执法门类"></el-table-column>
+    <el-table-column prop="address" label="执法证号"></el-table-column>
+    <el-table-column prop="address" label="转入单位"></el-table-column>
+    <el-table-column prop="address" label="转入时间"></el-table-column>
+    <el-table-column prop="address" label="转入执法门类"></el-table-column>
+    <el-table-column prop="address" label="调岗状态"></el-table-column>
+    <el-table-column prop="address" label="操作人"></el-table-column>
+    <el-table-column prop="address" label="操作时间"></el-table-column>
+  </el-table>
 </template>
 
 <script>
@@ -7,12 +21,24 @@
         name: "adjustingPosts",//调岗组件
         data(){
           return {
-
+            tableData: [{
+              date: '2016-05-02',
+              name: '王小虎',
+              address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+              date: '2016-05-04',
+              name: '王小虎',
+              address: '上海市普陀区金沙江路 1517 弄'
+            }, {
+              date: '2016-05-01',
+              name: '王小虎',
+              address: '上海市普陀区金沙江路 1519 弄'
+            }]
           }
         }
     }
 </script>
 
 <style scoped>
-
+  @import "./../../css/personManage.less";
 </style>
--- src/components/personComponents/punishment.vue
+++ src/components/personComponents/punishment.vue
@@ -1,18 +1,54 @@
 <template>
-
+  <div>
+    <el-row>
+      <el-button type="primary" icon="el-icon-plus"  size="mini" round>新增</el-button>
+      <el-button type="danger" icon="el-icon-delete" size="mini" round>删除</el-button>
+    </el-row>
+    <el-table
+      :data="tableData"
+      border
+      style="width: 100%"
+      @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55"></el-table-column>
+      <el-table-column prop="name" label="姓名"></el-table-column>
+      <el-table-column prop="name" label="惩罚类型"></el-table-column>
+      <el-table-column prop="date" label="惩罚日期"></el-table-column>
+      <el-table-column prop="address" label="惩罚机构"></el-table-column>
+      <el-table-column prop="address" label="惩罚原因"></el-table-column>
+      <el-table-column prop="address" label="操作项"></el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
     export default {
         name: "punishment",//惩罚组件
-        data(){
-          return {
-
-          }
+      data(){
+        return {
+          tableData: [{
+            date: '2016-05-02',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄'
+          }, {
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1517 弄'
+          }, {
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄'
+          }],
+          multipleSelection: []
         }
+      },
+      methods:{
+        handleSelectionChange(val) {
+          this.multipleSelection = val;
+        }
+      }
     }
 </script>
 
 <style scoped>
-
+  @import "./../../css/personManage.less";
 </style>
