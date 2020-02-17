72fc1cd0c9a6b9251956fb83093ac22bbee2243e
张敏慧 <zhangminhui@catsti.com>
2020-02-17 17:35:58 +0800

    更新

--- src/page/home.vue
+++ src/page/home.vue
@@ -1,36 +1,47 @@
 <template>
-    <div >
-        <div v-if="currentHome == 'xboot'">
-            系统管理首页
-        </div>
-        <div v-if="currentHome == 'caseHandle'">
-            案件办理首页
-        </div>
-        {{currentHome}}
+  <div class="full">
+    <div v-if="currentHome == 'xboot'" class="full">
+      系统管理首页
     </div>
+    <div v-if="currentHome == 'caseHandle'" class="full">
+      <!-- 案件办理首页 -->
+      <caseHome></caseHome>
+    </div>
+    <!-- {{currentHome}} -->
+  </div>
 </template>
 <script>
 import iLocalStroage from "@/js/localStroage";
+import caseHome from '@/page/caseHome.vue';
 export default {
-    data() {
-        return{
-            nav:iLocalStroage.get('headActiveNav')
-        }
-    },
-    computed:{
-        currentHome :function(){
-            console.log(iLocalStroage.get('headActiveNav'))
-            return iLocalStroage.get('headActiveNav')
-        }
-    },
-    methods:{
-
-    },
-    watch:{
-        nav(to,from) {
-            console.log(to);
-        }
+  components: {
+    caseHome
+  },
+  data() {
+    return {
+      nav: iLocalStroage.get('headActiveNav')
     }
+  },
+  computed: {
+    currentHome: function () {
+      console.log(iLocalStroage.get('headActiveNav'))
+      return iLocalStroage.get('headActiveNav')
+    }
+  },
+  methods: {
+
+  },
+  watch: {
+    nav(to, from) {
+      console.log(to);
+    }
+  }
 
 }
 </script>
+<style scoped>
+.full {
+  width: 100%;
+  height: 100%;
+}
+</style>
