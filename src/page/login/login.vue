3e245ff09d206e0fd6d39c6ff9d20677053cbbf1
张敏慧 <zhangminhui@catsti.com>
2020-02-26 16:50:16 +0800

    超时

--- src/page/login/login.vue
+++ src/page/login/login.vue
@@ -309,7 +309,7 @@ export default {
             setTimeout(() => {
               this.errorMessage = ""
             }, 3000)
-      }, 50000);
+      }, 300000);
       console.log('1111')
     },
     // 微信公众号
