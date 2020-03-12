a2c73257345fc0656bbede1d613cf53330cdab46
unknown <jingli@catsti.com>
2020-03-12 17:55:39 +0800

    修改接口

--- src/api/login.js
+++ src/api/login.js
@@ -9,52 +9,52 @@ let vm = new Vue();
  * @param {*} params.password  必填  密码
  */
 
+export function loginInApi(params) {
+  var data = new FormData();
+  data.append("username", params.username);
+  data.append("password", params.password);
+  data.append("grant_type", "password");
+  return request({
+    url: "/auth/oauth/token",
+    method: "POST",
+    data:data,
+    headers: {
+      "Authorization": "Basic Y2F0c2ljOmNhdHNpYw==",
+      "Content-Type": "multipart/form-data"
+    },
+    auth:{
+      username: 'catsic',
+      password: 'catsic'
+    },
+    showloading: true,
+    mimeType: "multipart/form-data",
+    processData: false,
+    showloading: true,
+    contentType: false,
+    // baseUrlType:1,
+    cancelToken: setCancelSource()
+  });
+}
+
 // export function loginInApi(params) {
-//   var data = new FormData();
-//   data.append("username", params.username);
-//   data.append("password", params.password);
-//   data.append("grant_type", "password");
+
+//   let data = vm.$qs.stringify({
+//     username: params.username,
+//     password: params.password,
+//     code: params.code,
+//     captchaId: params.captchaId
+//   })
+//   console.log(data)
 //   return request({
-//     url: "/auth/oauth/token",
+//     url: "/login",
 //     method: "POST",
+//     // contentType: 'multipart/form-data',
 //     data:data,
-//     headers: {
-//       "Authorization": "Basic Y2F0c2ljOmNhdHNpYw==",
-//       "Content-Type": "multipart/form-data"
-//     },
-//     auth:{
-//       username: 'catsic',
-//       password: 'catsic'
-//     },
 //     showloading: true,
-//     mimeType: "multipart/form-data",
-//     processData: false,
-//     showloading: true,
-//     contentType: false,
 //     baseUrlType:1,
 //     cancelToken: setCancelSource()
 //   });
 // }
-
-export function loginInApi(params) {
-
-  let data = vm.$qs.stringify({
-    username: params.username,
-    password: params.password,
-    code: params.code,
-    captchaId: params.captchaId
-  })
-  console.log(data)
-  return request({
-    url: "/login",
-    method: "POST",
-    // contentType: 'multipart/form-data',
-    data:data,
-    showloading: true,
-    baseUrlType:1,
-    cancelToken: setCancelSource()
-  });
-}
 
 
 //修改密码
@@ -67,7 +67,7 @@ export function resetPasswordApi(params) {
   })
   console.log(data)
   return request({
-    url: "/sys/user/resetPassWord",
+    url: "/system/sys/user/resetPassWord",
     method: "POST",
     contentType: 'multipart/form-data',
     data:data,
@@ -105,7 +105,7 @@ export function getCapImgSrcApi(captchaId) {
 //获取菜单
 export function getMenuApi() {
   return request({
-    url: "/sys/permission/getTreePermission",
+    url: "/system/sys/permission/getTreePermission",
     method: "get",
     baseUrlType:1,
     showloading: false,
