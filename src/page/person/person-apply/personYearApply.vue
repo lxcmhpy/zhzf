3da7696b370c48830dc4c3aaa9ca7f112a2388dc
zhaoxiang <zhaoxiang@catsti.com>
2020-03-16 10:19:35 +0800

    添加人员相关页面

--- src/page/person/person-apply/personYearApply.vue
+++ src/page/person/person-apply/personYearApply.vue
@@ -4,19 +4,46 @@
     <div class="searchPage toggleBox">
         <div class="handlePart">
             <el-form :inline="true" ref="userForm"  label-width="70px">
-                    <el-form-item label="执法机构" prop="organNp
-                    
-                    ">
-                        <el-input v-model="formInline.organNp" placeholder="回车可直接查询"></el-input>
+                <el-row>
+                    <el-form-item label=" " label-width="13px">
+                        <el-button type="primary" icon="el-icon-share" size="medium">批量年审</el-button>
+                        <el-button type="info"  icon="el-icon-refresh" size="medium"  @click="reset">重置查询项</el-button>
+                        <el-button type="warning" icon="el-icon-search" size="medium">查询</el-button>
                     </el-form-item>
-                    <el-form-item label="" label-width="13px">
-                        <el-button type="primary" icon="el-icon-search" size="medium">查询</el-button>
-                        <el-button type="success"  icon="el-icon-refresh" size="medium"  @click="reset">重置</el-button>
-                        <el-button type="info" icon="el-icon-plus" size="medium" @click="addOrgApply(0)">新增</el-button>
-                        <el-button type="danger" icon="el-icon-delete" size="medium" >删除</el-button>
+                </el-row> 
+                <el-row>  
+                     <el-form-item label="查询项" >
+                    <el-select >
+                        <el-option label="姓名" value="0"></el-option>
+                        <el-option label="执法证号" value="1"></el-option>
+                        <el-option label="所属机构" value="2"></el-option>
+                        <el-option label="执法门类" value="3"></el-option>
+                        <el-option label="职务" value="3"></el-option>
+                        <el-option label="年审年份" value="3"></el-option>
+                        <el-option label="年审成绩" value="3"></el-option>
+                    </el-select>
                     </el-form-item>
-
+                     <el-form-item label="姓名" >
+                        <el-input ></el-input>
+                    </el-form-item>
+                    <el-form-item label="执法证号" >
+                        <el-input ></el-input>
+                    </el-form-item>
+                    <el-form-item label="所属机构" >
+                        <el-input ></el-input>
+                    </el-form-item>
+                    <el-form-item label="执法门类" >
+                    <el-select >
+                        <el-option label="综合执法" value="0"></el-option>
+                        <el-option label="海事执法" value="1"></el-option>
+                        <el-option label="工程质量监督" value="2"></el-option>
+                        <el-option label="道路运政" value="3"></el-option>
+                    </el-select>
+                    </el-form-item>
+                 
+                </el-row>
             </el-form>
+            
         </div>
         <div class="tablePart">
             <el-table
@@ -25,15 +52,18 @@
                 style="width: 100%;height:100%"
             >
                 <el-table-column type="selection" align="center"></el-table-column>
-                <el-table-column prop="data" label="执法机构"></el-table-column>
-                <el-table-column prop="data" label="执法门类"></el-table-column>
-                <el-table-column prop="data" label="号段起"></el-table-column>
-                <el-table-column prop="data" label="号段止"></el-table-column>
-                <el-table-column prop="data" label="备注"></el-table-column>
+                <el-table-column prop="" label="姓名"></el-table-column>
+                <el-table-column prop="" label="性别"></el-table-column>
+                <el-table-column prop="" label="职务"></el-table-column>
+                <el-table-column prop="" label="执法证号"></el-table-column>
+                <el-table-column prop="" label="所属机构"></el-table-column>
+                <el-table-column prop="" label="执法门类"></el-table-column>
                 <el-table-column label="操作项" width="160">
                 <template slot-scope="scope" >
                     <div style="width:160px">
-                    <el-button type="text" @click="addOrgApply(1)">修改</el-button>
+                    <el-button type="text" @click="handleEdit(scope.$index, scope.row)">审批通过</el-button>
+                    <el-button type="text" @click="handleEdit(scope.$index, scope.row)">审批不通过</el-button>
+                    <el-button type="text" @click="handleEdit(scope.$index, scope.row)">查看</el-button>
                     </div>
                 </template>
                 </el-table-column>
@@ -77,7 +107,7 @@ export default {
         organRealNp: "",
         organCl: "",
       },
-      tableData: [{data:"测试数据"}], //表格数据
+      tableData: [], //表格数据
       defaultExpandedKeys: [], //默认展开的key
       currentPage: 1, //当前页
       pageSize: 10, //pagesize
@@ -95,6 +125,7 @@ export default {
     //执法号段列表查询
     getOrgList(val) {
  
+    alert("执行查询");
     },
     //更改每页显示的条数
     handleSizeChange(val) {
@@ -121,14 +152,15 @@ export default {
     },
 // 重置查询条件
   reset() {
+      alert("aaa");
       this.$refs["userForm"].resetFields();
     },
    
     //新增
-    addOrgApply(type) {
+    addOrgApply() {
       let parentNode = {
       };
-      this.$refs.addParagraph.showModal(type,parentNode);
+      this.$refs.addParagraph.showModal("1",parentNode);
     },
 
   },
@@ -140,4 +172,8 @@ export default {
   }
 };
 </script>
-<style src="@/assets/css/searchPage.scss" lang="scss" scoped></style>
+<!--
+<style lang="scss">
+@import "@/assets/css/systemManage.scss";
+</style>
+-->
---
+++ src/page/person/person-apply/personYearPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="com_searchAndpageBoxPadding" >
+    <div class="n_searchAndpageBox" style="height:1060px;">
+      <personInfoPage />
+    </div>
+  </div>
+</template>
+<script>
+import personYearApply from "./personYearApply"
+export default {
+  name: 'personDetailPage',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    personInfoPage
+  },
+}
+
+</script>
+<style lang="scss" scoped>
+@import "@/assets/css/personManageTitle.scss";
+</style>
