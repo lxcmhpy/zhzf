f514c2ea1d350493f7c14fd0c10b7b40feac36cf
yuyaqiang@catsti.com <yuyaqiang@catstic.com>
2020-03-23 10:25:17 +0800

    人员资料上传

--- src/components/personComponents/material.vue
+++ src/components/personComponents/material.vue
@@ -1,141 +1,43 @@
 <template>
     <div>
-        <div style="width:100%;float:left;margin-top:30px;margin-bottom:30px;">
-            <div style="margin-top:20px;margin-bottom:20px;margin-left:25px;margin-bottom:30px;">
-                <font style="font-size:25px;"><span class="titleflag"></span>身份证</font>
-            </div>
-            <div ref="idNoXX" class="block" style="margin-left:20px;float:left">
-                <el-upload
-                    action="#"
-                    multiple
-                    list-type="picture-card"
-                    :limit="2"
-                    :auto-upload="false"
-                    :on-exceed="handleExceed"
-                    :on-preview="handlePictureCardPreview"
-                    :on-remove="handleRemove"
-                    :on-change="handleChange">
-                    <div size="small" type="button" >点击上传</div>
-                    <div slot="tip" class="el-upload__tip" style="margin-left:160px;margin-top:25px;width:100%">
-                        只能上传jpg/png文件，且不超过500kb
-                    </div>
-                </el-upload>
-                <el-dialog :visible.sync="dialogVisible">
-                        <img width="100%" :src="dialogImageUrl" alt="">
-                </el-dialog>
-            </div>
-        </div>
-        <div style="width:100%;float:left;margin-top:30px;margin-bottom:30px;">
-            <div style="margin-top:20px;margin-bottom:20px;margin-left:25px;margin-bottom:30px;">
-                <font style="font-size:25px;"><span class="titleflag"></span>学历证明</font> 
-            </div>
-            <materialUpload ref="degreeXX"></materialUpload>
-        </div>
-        <div style="width:100%;float:left;margin-top:30px;margin-bottom:30px;">
-            <div style="margin-top:20px;margin-bottom:20px;margin-left:25px;margin-bottom:30px;">
-                <font style="font-size:25px;"><span class="titleflag"></span>编制证明</font> 
-            </div>
-            <materialUpload ref="staffXX"></materialUpload>
-        </div>
-        <div style="width:100%;float:left;margin-top:30px;margin-bottom:30px;">
-            <div style="margin-top:20px;margin-bottom:20px;margin-left:25px;margin-bottom:30px;">
-                <font style="font-size:25px;"><span class="titleflag"></span>其他证明</font>
-            </div>
-            <materialUpload ref="otherXX"></materialUpload>
-        </div>
+        <template>
+            <materialIdNo></materialIdNo>
+        </template>
+        <template>
+            <materialDegree></materialDegree>
+        </template>
+        <template>
+            <materialStaff></materialStaff>
+        </template>
+        <template>
+            <materialOther></materialOther>
+        </template>
     </div>
 </template>
 <script>
-import materialUpload from './materialUpload'
+import materialDegree from './materialDegree';
+import materialIdNo from './materialIdNo';
+import materialOther from './materialOther';
+import materialStaff from './materialStaff';
 export default {
     name:'material',//证明材料
     data(){
         return {
-            imageUrl: '',
-            fileList: [],
-            dialogImageUrl: '',
-            dialogVisible: false,
-            disabled: false
+            
         }
     },
     components:{
-        materialUpload
+        materialDegree,
+        materialIdNo,
+        materialOther,
+        materialStaff
     },
     methods:{
-        handleChange(file,fileList){
-            let _this=this
-            console.info("----------"+fileList.length)
-            if(fileList.length>0){//已经上传了文件
-                _this.$refs.idNoXX.children[0].children[1].style="margin-top:270px;"
-            }else{
-                _this.$refs.idNoXX.children[0].children[1].style="margin-top:-10px;"
-            }
-            if(fileList.length>1){//文件上传了多个时
-                for(var i=1;i<fileList.length;i++){
-                    _this.$refs.idNoXX.children[0].children[0].children[i].style="margin-left:120px;"
-                }
-            }
-        },
-        handleRemove(file,fileList) {
-            let _this=this
-            if(fileList.length>0){//已经上传了文件
-                _this.$refs.idNoXX.children[0].children[1].style="margin-top:270px;"
-            }else{
-                _this.$refs.idNoXX.children[0].children[1].style="margin-top:-10px;"
-            }
-        },
-        handlePreview(file) {
-            console.log(file);
-        },
-        handleExceed(files, fileList) {
-            this.$message.error(`当前限制选择 2 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
-        },
-        beforeRemove(file, fileList) {
-            return this.$confirm(`确定移除 ${ file.name }？`);
-        },
-        handlePictureCardPreview(file) {
-            this.dialogImageUrl = file.url;
-            this.dialogVisible = true;
-        },
+        
         
     }
 }
 </script>
-<style lang="scss">
-@import "@/assets/css/personManage.scss";
-.el-upload el-upload--picture-card{
-    float:left;
-    margin-top:160px;
-}
-.el-upload--picture-card {
-    background-color: #fbfdff;
-    border: 1px dashed #c0ccda;
-    border-radius: 6px;
-    -webkit-box-sizing: border-box;
-    box-sizing: border-box;
-    width: 120px;
-    height: 45px;
-    line-height: 45px;
-    vertical-align: top;
-    float:left;
-    margin-top:-10px;
-    margin-left:20px;
-}
-.el-upload-list--picture-card .el-upload-list__item {
-    overflow: hidden;
-    background-color: #fff;
-    border: 1px solid    #c0ccda;
-    border-radius: 6px;
-    -webkit-box-sizing: border-box;
-    box-sizing: border-box;
-    width: 390px;
-    height: 250px;
-    margin: 0 8px 8px 0;
-    display: inline-block;
-    margin-left: 120px;
-}
-.el-upload-list--picture-card .el-upload-list__item-actions span + span {
-    margin-left: 35px;
-    margin-top: 110px;
-}
+<style lang="scss" scoped>
+
 </style>
