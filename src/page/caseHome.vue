97f125a26c3e7e209766734d74fbe00a34d3b8e5
张敏慧 <zhangminhui@catsti.com>
2020-03-24 17:02:47 +0800

    更新

--- src/page/caseHome.vue
+++ src/page/caseHome.vue
@@ -79,17 +79,29 @@
       </div>
       <div class="padding22 tablebox">
         <el-table :data="tableData" stripe height="100%" highlight-current-row @current-change="clickCase">
-          <el-table-column :prop="moreFlag != 'unRecordCase' ? 'caseNumber'  :'tempNo' " label="案号" align="center" ></el-table-column>
+          <el-table-column :prop="moreFlag != 'unRecordCase' ? 'caseNumber'  :'tempNo' " label="案号" align="center"></el-table-column>
           <el-table-column prop="name" label="当事人" align="center"></el-table-column>
           <!-- <el-table-column prop="vehicleShipId" label="车/船号" align="center"></el-table-column> -->
           <el-table-column prop="caseCauseName" label="违法行为" align="center"></el-table-column>
           <!-- <el-table-column prop="acceptTime" label="受案时间" align="center"></el-table-column> -->
           <!-- <el-table-column prop="caseType" label="案件类型" align="center"></el-table-column> -->
           <el-table-column label="总处理时长" align="center">
-            <template></template>
+            <template slot-scope="scope">
+              <div>{{scope.row.caseDealTime||"-"}}</div>
+            </template>
           </el-table-column>
           <el-table-column prop="currentLinkName" label="当前环节" align="center"></el-table-column>
-          <el-table-column prop="caseStatus" label="当前状态" align="center"></el-table-column>
+          <el-table-column prop="caseStatus" label="当前状态" align="center">
+            <template slot-scope="scope">
+              <div v-if="scope.row.caseStatus=='办理中'" style="color:#2B313E">{{scope.row.caseStatus}}</div>
+              <div v-if="scope.row.caseStatus=='待归档'" style="color:#2B313E">{{scope.row.caseStatus}}</div>
+              <div v-if="scope.row.caseStatus=='待审批'" style="color:#00B4A1">{{scope.row.caseStatus}}</div>
+              <div v-if="scope.row.caseStatus=='待审核'" style="color:#00B4A1">{{scope.row.caseStatus}}</div>
+              <div v-if="scope.row.caseStatus=='审核中'" style="color:#00B4A1">{{scope.row.caseStatus}}</div>
+              <div v-if="scope.row.caseStatus=='未立案'" style="color:#E84241">{{scope.row.caseStatus}}</div>
+              <div v-if="scope.row.caseStatus=='审批中'" style="color:#0074F5">{{scope.row.caseStatus}}</div>
+            </template>
+          </el-table-column>
         </el-table>
       </div>
 
@@ -176,12 +188,27 @@
           </div>
         </el-form>
         <ul class="wfxwList">
-          <li v-for="item in caseList" :key="item.id" @click="caseRecord(item)"><span class="bull">&bull;</span>{{item.strContent}}</li>
+          <li v-for="item in caseList" :key="item.id" @click="caseRecord(item)">
+              <span class="bull">&bull;</span>
+            <el-tooltip class="item" effect="light" placement="top-start">
+              <div slot="content" style="max-width:250px">{{item.strContent}}</div>
+              <span>{{item.strContent}}</span>
+            </el-tooltip>
+          </li>
+
         </ul>
 
         <center>
           <el-button size="small" @click="caseRecordMore()">查看更多</el-button>
         </center>
+        <el-button type="text" @click="table = true">打开嵌套表格的 Drawer</el-button>
+        <el-drawer title="我嵌套了表格!" :visible.sync="table" direction="rtl" size="50%">
+          <el-table :data="gridData">
+            <el-table-column property="date" label="日期" width="150"></el-table-column>
+            <el-table-column property="name" label="姓名" width="200"></el-table-column>
+            <el-table-column property="address" label="地址"></el-table-column>
+          </el-table>
+        </el-drawer>
 
       </div>
 
@@ -206,6 +233,24 @@ export default {
   },
   data() {
     return {
+      gridData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }],
+      table: false,
       activeName: 'first',
       waitDealSearch: '',
       unRecordCaseSearch: '',
@@ -380,6 +425,14 @@ export default {
       data.current = this.currentPage;
       data.size = this.pageSize;
       this.getCaseList(data)
+      this.tableData.forEach(element => {
+        let nd = 1000 * 24 * 60 * 60;
+        let endTime = element.closeDate || new Date()
+        element.caseDealTime = endTime - new Date(element.acceptTime);
+        let day = element.caseDealTime / nd;
+        day = Math.floor(day)
+        console.log(day, '天', endTime, element.acceptTime, element.caseDealTime)
+      });
     },
 
     // 信息查验
@@ -483,6 +536,17 @@ export default {
     this.getTotal('1');
     this.getTotal('2');
     this.getTotal('3');
+    console.log('userinfo', iLocalStroage.gets("userInfo").roles[0].name)
+
+    let role = iLocalStroage.gets("userInfo").roles[0].name
+    if (role.indexOf("负责人") != -1) {
+      console.log('yes')
+      this.activeName = 'fourth';
+      let searchData = {
+        flag: 3
+      }
+      this.getCaseList2(searchData);
+    }
   }
 };
 </script>
