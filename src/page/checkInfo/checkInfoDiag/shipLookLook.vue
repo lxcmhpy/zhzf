86b485c6ee1ad728d4c94703bce5235d9881b6ef
hanwenjing <hanwenjing@catsti.com>
2020-03-29 16:53:58 +0800

    信息查验

---
+++ src/page/checkInfo/checkInfoDiag/shipLookLook.vue
@@ -0,0 +1,145 @@
+<template>
+  <el-dialog
+    :title="dialogTitle"
+    :visible.sync="visible"
+    @close="closeDialog"
+    :close-on-click-modal="false"
+    width="50%"
+    append-to-body
+  >
+    <el-form :model="cblCyForm" :rules="rules" ref="cblCyForm" class="errorTipForm">
+        <div align="center">
+            <table class="tg" style="width:900px;">
+                <tr>
+                    <td class="tg-0lax" colspan="6">基本信息</td>
+                </tr>
+                <tr>
+                    <td class="tg-0pky" style="width:120px;">中文船名</td>
+                    <td class="tg-0pky" style="width:220px;">{{cblCyForm.CHUANJI}</td>
+                    <td class="tg-0pky" style="width:120px;">英文船名</td>
+                    <td class="tg-0pky" colspan="3"></td>
+                </tr>
+                <tr>
+                    <td class="tg-0pky">船舶识别号</td>
+                    <td class="tg-0pky"></td>
+                    <td class="tg-0pky">IMO编号</td>
+                    <td class="tg-0pky" colspan="3">{{cblCyForm.IMOHAO}}</td>
+                </tr>
+                <tr>
+                    <td class="tg-0pky">初次登记号</td>
+                    <td class="tg-0pky">{{cblCyForm.CHUANBODENGJIHAO}}</td>
+                    <td class="tg-0pky">MMSI编号</td>
+                    <td class="tg-0pky" colspan="3"></td>
+                </tr>
+                <tr>
+                    <td class="tg-0pky">船籍港名称</td>
+                    <td class="tg-0pky">{{cblCyForm.CHUANJIGANG}}</td>
+                    <td class="tg-0pky">船籍港代码</td>
+                    <td class="tg-0pky" colspan="3"></td>
+                </tr>
+                <tr>
+                    <td class="tg-0pky">船舶种类名称</td>
+                    <td class="tg-0pky">{{cblCyForm.CHUANBOLEIXING}</td>
+                    <td class="tg-0pky">船舶种类代码</td>
+                    <td class="tg-0pky" colspan="3">{{cblCyForm.CHUANBOLEIXING}}</td>
+                </tr>
+                <tr>
+                    <td class="tg-0pky">船舶总长</td>
+                    <td class="tg-0pky"></td>
+                    <td class="tg-0pky">型宽</td>
+                    <td class="tg-0pky" style="width:120px;"></td>
+                    <td class="tg-0pky" style="width:100px;">型深</td>
+                    <td class="tg-0pky"></td>
+                </tr>
+                <tr>
+                    <td class="tg-0pky">总吨</td>
+                    <td class="tg-0pky">{{cblCyForm.CHUANBOZONGDUN}}</td>
+                    <td class="tg-0pky">净吨</td>
+                    <td class="tg-0pky">{{cblCyForm.ZAIXIANGDINGE}}</td>
+                    <td class="tg-0pky">参考载重吨</td>
+                    <td class="tg-0pky">{{cblCyForm.ZAIZHONGDUN}}</td>
+                </tr>
+                <tr>
+                    <td class="tg-0pky">船舶所有人名称</td>
+                    <td class="tg-0pky">{{cblCyForm.CHUANBOSUOYOUREN}}</td>
+                    <td class="tg-0pky">船舶经营人名称</td>
+                    <td class="tg-0pky" colspan="3">{{cblCyForm.CHUANBOJINGYINGREN}}</td>
+                </tr>
+                <tr>
+                    <td class="tg-0lax" colspan="6"><span style="font-weight:700">其他信息</span></td>
+                </tr>
+                <tr>
+                    <td class="tg-0pky">船舶呼号</td>
+                    <td class="tg-0pky"></td>
+                    <td class="tg-0pky">核定抗风等级</td>
+                    <td class="tg-0pky">{{cblCyForm.JINGYINGRENXUKEZHENGHAOMA}}</td>
+                    <td class="tg-0pky">夏季满载吃水</td>
+                    <td class="tg-0pky"></td>
+                </tr>
+                <tr>
+                    <td class="tg-0pky">货舱数量</td>
+                    <td class="tg-0pky"></td>
+                    <td class="tg-0pky">压载舱数</td>
+                    <td class="tg-0pky"></td>
+                    <td class="tg-0pky">水密横舱壁数</td>
+                    <td class="tg-0pky"></td>
+                </tr>
+                <tr>
+                    <td class="tg-0pky">最小干舷</td>
+                    <td class="tg-0pky"></td>
+                    <td class="tg-0pky">甲板层数</td>
+                    <td class="tg-0pky"></td>
+                    <td class="tg-0pky"></td>
+                    <td class="tg-0pky"></td>
+                </tr>
+                <tr>
+                    <td class="tg-0pky">主机数量</td>
+                    <td class="tg-0pky">{{cblCyForm.ZHUJISHULIANG}}</td>
+                    <td class="tg-0pky">主机功率</td>
+                    <td class="tg-0pky">{{cblCyForm.ZHUJIGONGLU}}</td>
+                    <td class="tg-0pky"></td>
+                    <td class="tg-0pky"></td>
+                </tr>
+                <tr>
+                    <td class="tg-0pky">主机编号</td>
+                    <td class="tg-0pky"></td>
+                    <td class="tg-0pky">主机型号</td>
+                    <td class="tg-0pky"></td>
+                    <td class="tg-0pky"></td>
+                    <td class="tg-0pky"></td>
+                </tr>
+            </table>
+        </div>
+    </el-form>
+  </el-dialog>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      visible: false,
+      cblCyForm: {
+      },
+      dialogTitle: "",
+    };
+  },
+  methods: {
+    shipLookLook(data) {
+      this.visible = true;
+      this.dialogTitle = "船舶详情";
+    },
+    //关闭弹窗的时候清除数据
+    closeDialog() {
+      this.visible = false;
+      this.$refs["cblCyForm"].resetFields();
+    },
+  }
+};
+</script>
+<style lang="scss">
+@import "@/assets/css/systemManage.scss";
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{font-family:Arial, sans-serif;font-size:16px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:black;}
+.tg .tg-0pky{border-color:inherit;text-align:center;vertical-align:center;height: 25px;}
+.tg .tg-0lax{background-color:#c0c0c0;border-color:inherit;font-size:20px;text-align:left;vertical-align:center;height: 30px;}
+</style>
--- src/page/checkInfo/vehicleLine.vue
+++ src/page/checkInfo/vehicleLine.vue
@@ -1,3 +1,129 @@
 <template>
-    <div>客运线路标志牌</div>
+    <div class="check">
+        <div class="top shadow">
+            <div class="title_content">
+                <div class="title_back float" @click="goBack">&lt;返回</div>
+                <div class="float title_box title">客运线路标志牌</div>
+            </div>
+        </div>
+        <el-form :inline="true" :model="checkData">
+            <div class="center shadow">
+                <div class="center_content">
+                <span class="titleflag">
+                </span>
+                <span class="title">查验条件</span>
+                </div>
+                <div class="title_box">
+                    <el-form-item label="车牌颜色" class="margin28" >
+                        <el-select v-model="checkData.color" placeholder="请选择">
+                            <el-option
+                                v-for="(item,i) in sfList"
+                                :key="i"
+                                :label="item.value"
+                                :value="item.label">
+                            </el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="车牌号码">
+                        <el-input style="width:400px;" v-model="checkData.cph" placeholder="输入车牌号码"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                    <el-button type="primary" @click="getVehicleCheck" size="medium">查询</el-button>
+                    </el-form-item>
+                </div>
+            </div>
+        </el-form>
+        <div class="bottom shadow">
+            <div class="center_content">
+                <span class="titleflag">
+                </span>
+                <span class="title">查验结果</span>
+            </div>  
+            <div class="tablePart" align="center">
+                <el-table :data="tableData" stripe resizable border style="width: 100%;height:100%;" >
+                <el-table-column prop="VehicleNo" label="车牌号" align="center"></el-table-column>
+                <el-table-column prop="SignId" label="标识牌号" align="center"></el-table-column>
+                <el-table-column prop="ValidDate" label="标识牌有效起始日期" align="center"></el-table-column>
+                <el-table-column prop="OperLineName" label="线路名称" align="center"></el-table-column>
+                <el-table-column prop="LineOpStatusCode" label="客运班线类型" align="center"></el-table-column>
+                <el-table-column prop="LineValidBegin" label="线路有效起始日期" align="center"></el-table-column>
+                <el-table-column label="操作" align="center">
+                        <template slot-scope="scope" >
+                            <div>
+                            <el-button type="text" @click="vehicleLookLook(scope.$index, scope.row)">查看</el-button>
+                            </div>
+                        </template>
+                </el-table-column>
+                </el-table>
+            </div>
+                <vehicleLookLook ref="vehicleLookLookRef" @uploadaaa="getVehiclexList()"></vehicleLookLook>
+            </div>
+        </div>
 </template>
+
+<script>
+import vehicleLookLook from "@/page/checkInfo/checkInfoDiag/vehicleLookLook";
+import { crewCheckApiKyxlbsp } from "@/api/checkInfo";
+export default {
+  data() {
+    return {
+      checkData: {
+        color: '',
+        cph: '',
+      },
+      tableData: [{
+        VehicleNo: 1,
+        SignId: 11,
+        ValidDate: 1,
+        OperLineName: 1,
+        LineOpStatusCode: 1,
+        LineValidBegin: 1
+      }],
+      searchList: [],
+      showFlag: true,
+      checkFlag: false,
+      loading: true,
+    }
+  },
+  components: {
+    vehicleLookLook,
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit!');
+    },
+    // 返回
+    goBack() {
+      this.$router.go(-1);//返回上一层
+    },
+    getVehicleCheck() {
+      let data = {
+        VehicleNo:this.checkData.color,
+        PlateColorCode: this.checkData.cph
+      };
+      this.searchList = [];
+      this.checkFlag = true;
+      let _this = this
+      this.$store.dispatch("crewCheckApiKyxlbsp", data).then(res => {
+        _this.tableData = res.data.records;
+      });
+    },
+    changeType() {
+      this.clearData()
+    },
+    //查看
+    vehicleLookLook(index, row) {
+      this.$refs.vehicleLookLookRef.vehicleLookLook(row);
+    },
+    // 清空数据
+    clearData() {
+      this.checkData.color = '';
+      this.checkData.cph = '';
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+@import "@/assets/css/checkInfo.scss";
+</style>
