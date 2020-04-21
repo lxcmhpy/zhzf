ffa3596d0a517902d806b673d8dee1cb26bfed05
张敏慧 <zhangminhui@catsti.com>
2020-04-21 19:19:49 +0800

    更新移送样式

--- src/page/caseHandle/cooperateCases/addPage.vue
+++ src/page/caseHandle/cooperateCases/addPage.vue
@@ -125,13 +125,25 @@
           </el-upload>
           <ul class="file-upload">
             <!-- <li v-for="item in fileListArr" :key="item.id" v-on:mouseover="mouseoverFile()" v-on:mouseout="mouseoutFile()"> -->
-            <li v-for="item in fileListArr" :key="item.id" v-on:mouseover="mouseoverFile()" v-on:mouseout="mouseoutFile()">
+            <li class="file-upload-li" v-for="item in fileListArr" :key="item.id" v-on:mouseover="mouseoverFile()" v-on:mouseout="mouseoutFile()">
               <i class="el-icon-document"></i>
               {{item.fileName}}
-              <span style="float:right;margin-right:8px"><i @click="deleteFile(item)" class="el-icon-close"></i></span>
+              <span style="float:right;margin-right:8px" class="del-icon"><i @click="deleteFile(item)" class="el-icon-close"></i></span>
             </li>
           </ul>
-
+<!-- <el-upload
+  class="upload-demo"
+  action="https://jsonplaceholder.typicode.com/posts/"
+  :http-request="uploadPaymentVoucher"
+  :on-preview="handlePreview"
+  :on-remove="handleRemove"
+  multiple
+  :limit="3"
+  :on-exceed="handleExceed"
+  :file-list="fileList">
+  <el-button size="small" type="primary">点击上传</el-button>
+  <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+</el-upload> -->
 
         </el-form-item>
         <el-form-item label="备注">
@@ -558,7 +570,20 @@ export default {
     },
     mouseoverFile() {
       this.showDelete = true
-    }
+    },
+    
+    //  handleRemove(file, fileList) {
+    //     console.log(file, fileList);
+    //   },
+      handlePreview(file) {
+        console.log(file);
+      },
+      handleExceed(files, fileList) {
+        this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+      },
+      beforeRemove(file, fileList) {
+        return this.$confirm(`确定移除 ${ file.name }？`);
+      }
   },
   created() {
     this.getAllOrgan("root");
