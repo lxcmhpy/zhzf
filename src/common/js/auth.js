9f520cc6948bc652df7b82535f5e6bfc892efe24
chenxiaoxiao <chenxiaoxiao@catsti.com>
2020-04-26 11:22:50 +0800

    cxx

--- src/common/js/auth.js
+++ src/common/js/auth.js
@@ -12,7 +12,7 @@ export function getToken(tokenName) {
  * @param {*} expires 如果传入Number，那么单位为天，你也可以传入一个Date对象，表示有效期至Date指定时间
  */
 export function setToken(tokenName, tokenVal, expires = 1) {
-  let date = new Date()
+  let date = new Date() 
   date.setDate(date.getDate()+1)//1表示1天
   return iLocalStroage.setExpired(tokenName, tokenVal, date.getTime().toString());
 }
