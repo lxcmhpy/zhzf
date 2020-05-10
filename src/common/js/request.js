3657262dc0b00df57c587e0eef9e586dc359eda2
张敏慧 <zhangminhui@catsti.com>
2020-05-10 10:41:01 +0800

    g

--- src/common/js/request.js
+++ src/common/js/request.js
@@ -53,20 +53,22 @@ service.interceptors.request.use(
       config["responseType"] = config.responseType
     }
     // get方法
-    // if(config.method=== 'get'){
-    // //   debugger
-    // console.log('get',config)
+    if(config.method=== 'get'){
+    //   debugger
+    console.log('get',config)
     // if(config.params){
-    //   config.params=qs.stringify(config.params)
+      // config.params=qs.stringify(config.params)
+      // config.params=async qs.stringify(config.params)
+      // config.params=await qs.stringify(config.params)
     // }
-    // }
+    }
     // post方法
     if (config.method === 'post') {
       console.log('post', config)
       console.log('config.data', config.data)
-      console.log('config.data.qs', qs.stringify({ a: ['b', 'c', 'd'] }))
+      console.log('config.data.qs',  qs.stringify({ a: ['b', 'c', 'd'] }))
 
-      // config.data=qs.stringify(config.data)//报400
+      // config.data=qs.stringify(config.data)//有些post方法如findByCondition在接口文件中已处理
       // console.log('postdeal', config)
     }
     // config["Content-Type"] = config.contentType ? config.contentType : "application/x-www-form-urlencoded"
