98d64d57d2ea8a9f181bc6c34ee59bc56df210a0
张敏慧 <zhangminhui@catsti.com>
2020-05-25 17:43:45 +0800

    行政检查

--- src/page/inspection/record-manage/writeRecord.vue
+++ src/page/inspection/record-manage/writeRecord.vue
@@ -3,8 +3,21 @@
     <div class="searchAndpageBox">
 
       标题{{formData.title}}
-
       <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+        <el-form-item v-for="(item,index) in formData.formList" :key="index" :label="item.label" :prop="'formList[+'+index+'].value'" :rules="{
+      required: true, message: '域名不能为空', trigger: 'blur'}">
+
+          prop:{{'item.' + index + '.value'}}
+          value:{{item.value}}
+
+          <el-input v-model="item.value"></el-input>
+        </el-form-item>
+
+        <template v-for="(person,index) in Form.List">
+          <el-form-item :prop="'List.'+index + '.code'" :key="person.key" :rules="rules">
+            <el-input v-model="person.code"></el-input>
+          </el-form-item>
+        </template>
 
       </el-form>
     </div>
@@ -15,20 +28,27 @@ export default {
   data() {
     return {
       ruleForm: {
-        name:'',
+        value1: '',
+        value2: '',
       },
       formData: {
         title: '5月21日检查记录',
         formList: [{
           formTitle: '',
           type: 'input',
-          label: '',
+          label: '姓名',
           prop: 'name',
-
-        }]
+          value: '1'
+        }, {
+          formTitle: '',
+          type: 'input',
+          label: '电话',
+          prop: 'name',
+          value: '2'
+        },]
 
       },
-      
+      rules: {}
 
     }
   },
