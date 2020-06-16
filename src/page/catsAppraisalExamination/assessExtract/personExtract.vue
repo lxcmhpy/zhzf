bb20c4b067ec9ea5e3cf7b9b49179f380880672d
xdy <291032963@qq.com>
2020-06-16 18:02:42 +0800

    5

--- src/page/catsAppraisalExamination/assessExtract/personExtract.vue
+++ src/page/catsAppraisalExamination/assessExtract/personExtract.vue
@@ -22,7 +22,7 @@
               <el-button type="primary" size="medium" icon="el-icon-search" @click="searchData">查询</el-button>
             </el-form-item>
             <el-form-item>
-              <el-button type="primary" size="medium" icon="el-icon-plus"  @click="randomSamplingStaff">新增</el-button>
+              <el-button type="primary" size="medium" icon="el-icon-plus"  @click="randomSamplingStaff">随机抽取</el-button>
             </el-form-item>
 
           </el-form>
@@ -30,16 +30,17 @@
 
         <div class="extractPage">
           <div class="com_extract_top" >
-            <el-transfer width="100%" :titles="['案件列表', '已抽取案件']" v-model="value" :data="personData">
+            <el-transfer width="100%" :titles="['人员列表', '已抽取人员']" v-model="value"
+                         :button-texts="['取消', '抽取']" :data="personList"
+                         @change="handleChange">
                 <span slot-scope="{ option }" >
                     <ul class="transfer-list" >
-                        <li><span>案件编号{{ option.key }} - {{ option.label }}</span></li>
-                        <li><span>案件类型{{ option.key }} - {{ option.label }}</span></li>
-                        <li><span>业务领域{{ option.key }} - {{ option.label }}</span></li>
+                        <li><span>姓名:{{ option.staffName }} </span></li>
+                        <li><span>证件号:{{ option.maritimeNo }} </span></li>
                     </ul>
                 </span>
-              <el-button class="transfer-footer" slot="left-header" size="small" >操作</el-button>
-              <el-button class="transfer-footer" slot="right-header" size="small">操作</el-button>
+<!--              <el-button class="transfer-footer" slot="left-header" size="small" @click="leftClick">操作</el-button>-->
+<!--              <el-button class="transfer-footer" slot="right-header" size="small" @click="rightClick">操作</el-button>-->
 
             </el-transfer>
           </div>
@@ -50,25 +51,13 @@
 </template>
 <script>
   import { mixinsCommon } from "@/common/js/mixinsCommon";
-  import {findPykhStaffByPage,randomSamplingStaffByPage,findListVoByBatch,findAllDepartment} from "@/api/catsAppraisalExamPersonUpload.js";
+  import {findPykhStaffByPage,randomSamplingStaffByPage,findListVoByBatch,findAllDepartment,submitProStaff} from "@/api/catsAppraisalExamPersonUpload.js";
   import iLocalStroage from '@/common/js/localStroage';
 
   export default {
     data() {
-      const generateData = _ => {
-        const data = [];
-        for (let i = 1; i <= 15; i++) {
-          data.push({
-            key: i,
-            label: `备选项 ${ i }`,
-            disabled: i % 4 === 0
-          });
-        }
-        return data;
-      };
       return {
-        personData: generateData(),
-        value: [1, 4],
+        value: [],
         search:{
           OId:"",
           batchId:""
@@ -83,12 +72,59 @@
     },
     methods:{
       fetchData(data){
+        data.size=1000;
+        data.current=1;
         console.info("查询参数：",data)
         findPykhStaffByPage(data).then(res=>{
           console.info("根据条件分页查询人员列表:",res);
           if(res.code==200){
-            this.personList=res.data.records;
+            var personlist=[];
+            for(var i=0;i<res.data.records.length;i++){
+              var maritimeNo=res.data.records[i].maritimeNo==null?'':res.data.records[i].maritimeNo+",";
+              var provinceNo=res.data.records[i].provinceNo==null?'':res.data.records[i].provinceNo+",";
+              var ministerialNo=res.data.records[i].ministerialNo==null?'':res.data.records[i].ministerialNo+",";
+              personlist.push({
+                key: i,
+                label: res.data.records[i].staffName,
+                staffName:res.data.records[i].staffName,
+                maritimeNo: maritimeNo+provinceNo+ministerialNo,
+                staffId:res.data.records[i].staffId,
+                disabled: res.data.records[i].staffStatus==0?false:true
+              });
+            }
+            this.personList=personlist;
           }
+          console.info('psersonlist:',this.personList);
+        });
+      },
+      handleChange(value, direction, movedKeys) {
+        console.log(value, direction, movedKeys);
+        var ids=[];
+        var personList=this.personList;
+        console.info("personlist:",personList)
+        // for (var i =0 ;i<personList.length;i++){
+        //   for(var j=0;j<value.length;j++){
+        //     console.info("抽取号：",value[j])
+        //     if(i!=value[j]){
+        //       ids.push(personList[i].staffId);
+        //     }
+        //   }
+        // }
+        for(var j=0;j<value.length;j++){
+          // for (var i =0 ;i<personList.length;i++){
+          //   if(i==value[j]){
+          //           ids.push(personList[i].staffId);
+          //   }
+          //   break;
+          // }
+          ids.push(personList[value[j]].staffId)
+        }
+        console.info("ids:",ids)
+        var submitProStaffData={};
+        submitProStaffData.idList=ids;
+        submitProStaffData.batchId=this.search.batchId;
+        submitProStaff(submitProStaffData,this.search.OId).then(res=>{
+          console.info("抽取结果",res);
         });
       },
       resetSearch(){
@@ -103,9 +139,16 @@
         this.fetchData(data);
       },
       randomSamplingStaff(){
-        randomSamplingStaffByPage({}).then(res=>{
-          console.info("部级随机抽取人员:",res);
-        })
+        if(this.search.batchId=="" || this.search.OId=="" ){
+          this.$message({
+            message: '请先选择机构和考核名称',
+            type: 'error'
+          })
+        }else{
+          randomSamplingStaffByPage(this.search.OId,this.search.batchId).then(res=>{
+            console.info("部级随机抽取人员:",res);
+          })
+        }
       },
       getBatch(){
         let batchData={}
