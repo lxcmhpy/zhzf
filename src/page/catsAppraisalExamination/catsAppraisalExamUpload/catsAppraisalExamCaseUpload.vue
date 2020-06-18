8963a3386b0a9e8b80e239f97ae4b5fab28ca5a3
unknown <jingli@catsti.com>
2020-06-18 16:45:17 +0800

    修改抽取

--- src/page/catsAppraisalExamination/catsAppraisalExamUpload/catsAppraisalExamCaseUpload.vue
+++ src/page/catsAppraisalExamination/catsAppraisalExamUpload/catsAppraisalExamCaseUpload.vue
@@ -80,8 +80,9 @@
                 <el-upload
                   class="upload-demo"
                   accept=".pdf"
+                  :show-file-list="false"
                   v-show="scope.row.caseStatus==1"
-                  :http-request="saveFile"
+                  :http-request="(params)=>saveFile(params,scope.row)"
                   action="https://jsonplaceholder.typicode.com/posts/"
                   multiple
                   :limit="1">
@@ -172,7 +173,7 @@
   import { mixinsCommon } from "@/common/js/mixinsCommon";
   import {findPykhCaseByPage,importCase,saveOrUpdateCaseInfo,deleteCaseInfo } from "@/api/catsAppraisalExamCaseUpload.js";
   import {findListVoByBatch,findAllDepartment} from "@/api/catsAppraisalExamPersonUpload.js";
-  import viewNotice from "../noticeManage/viewNotice"; 
+  import viewNotice from "../noticeManage/viewNotice";
   import iLocalStroage from '@/common/js/localStroage';
 
   export default {
@@ -215,10 +216,12 @@
       }
     },
     methods:{
-      saveFile(param) {
+      saveFile(param, row) {
+          debugger;
         console.log(param);
       },
       view(row){
+          debugger;
         let routerData = {
           storageId: row.storageId
         };
