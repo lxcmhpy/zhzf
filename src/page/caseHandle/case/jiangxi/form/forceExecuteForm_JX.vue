a0e9facd83355efce27f085565b472c67fd466d2
wangyiming <18513310529@163.com>
2020-07-13 15:40:14 +0800

    强制执行

--- src/page/caseHandle/case/jiangxi/form/forceExecuteForm_JX.vue
+++ src/page/caseHandle/case/jiangxi/form/forceExecuteForm_JX.vue
@@ -164,7 +164,11 @@
               </el-table-column>
               <el-table-column prop="status" label="状态" align="center">
                 <template slot-scope="scope">
-                  <span v-if="scope.row.status == '1' || scope.row.status == '2'">已完成</span>
+                  <span v-if="scope.row.status == '1' || scope.row.status == '2'">
+                    <template v-if="scope.row.docProcessStatus=='待审批'">待审批</template>
+                    <template v-if="scope.row.docProcessStatus=='审批中'">审批中</template>
+                    <template v-if="scope.row.docProcessStatus==''|| scope.row.docProcessStatus=='已完成'">已完成</template>
+                  </span>
                   <span v-if="scope.row.status == '0'">未完成</span>
                   <span v-if="scope.row.status == ''"></span>
                 </template>
