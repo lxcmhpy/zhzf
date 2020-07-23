dd80fee1cff8227dcb2eae59197e1eb2c7ab2422
qiangsf <823201662@qq.com>
2020-07-23 15:02:22 +0800

    feat(jk-basehmap/): 地图组件添加基础控件

---
+++ src/components/jk-controlsMap/src/index.vue
@@ -0,0 +1,26 @@
+<script>
+import BaseHMap from "@/components/jk-baseHMap"
+export default {
+  components: {
+    BaseHMap
+  },
+  methods: {
+    render
+  },
+  render() {
+    return (
+      <div class="jk-controlsMap">
+        <BaseHMap />
+      </div>
+    )
+  }
+}
+</script>
+
+<style lang="scss">
+.jk-controlsMap {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+</style>
--- src/page/lawSupervise/supervisePage/kshjg/jiangXiMap.vue
+++ src/page/lawSupervise/supervisePage/kshjg/jiangXiMap.vue
@@ -1,12 +1,29 @@
 <template>
-  <JkBaseHMap />
+  <div class="jiangXiMap">
+    <JkBaseHMap />
+  </div>
 </template>
 
 <script>
-import JkBaseHMap from "@/components/jk-baseHMap"
+import JkBaseHMap from "@/components/jk-baseHMap";
 export default {
   components: {
     JkBaseHMap
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.jiangXiMap {
+  width: 100%;
+  height: 100%;
+}
+</style>
