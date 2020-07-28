ddb9951bfcebf15f0fbdb559b35476277e3cf67e
qiangsf <823201662@qq.com>
2020-07-28 10:54:17 +0800

    feat(jk-basehmap/): 封装地图打点函数

--- src/page/lawSupervise/supervisePage/kshjg/jiangXiMap.vue
+++ src/page/lawSupervise/supervisePage/kshjg/jiangXiMap.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="jiangXiMap">
     <JkControlsMap
+      @init="init"
       @handleNodeClick="handleNodeClick"
       @handleChange="handleChange"
-      :config="config" />
+      :config="config"
+      :center="center"
+    />
   </div>
 </template>
 
@@ -16,30 +19,34 @@ export default {
   },
   data() {
     return {
+      page: null,
+      map: null,
+      zoom: 8,
+      center: [12118909.300259633, 4086043.1061670054],
       config: {
         treeData: {
           title: "搜执法人员、执法机构",
           imgUrl: "/static/images/img/lawSupervise/icon_renyuan.png",
           option: [
-            {
-              label: '固原综合执法支队',
-              children: [{
-                label: '执法人员',
-              },{
-                label: '执法车辆',
-              },{
-                label: '执法船舶',
-              },{
-                label: '德隆综合执法大队',
-                children: [{
-                  label: '执法人员',
-                },{
-                  label: '执法车辆',
-                },{
-                  label: '执法船舶',
-                },]
-              }]
-            },
+            // {
+            //   label: '固原综合执法支队',
+            //   children: [{
+            //     label: '执法人员',
+            //   },{
+            //     label: '执法车辆',
+            //   },{
+            //     label: '执法船舶',
+            //   },{
+            //     label: '德隆综合执法大队',
+            //     children: [{
+            //       label: '执法人员',
+            //     },{
+            //       label: '执法车辆',
+            //     },{
+            //       label: '执法船舶',
+            //     },]
+            //   }]
+            // },
           ],
         },
         popoverData: {
@@ -116,21 +123,38 @@ export default {
     }
   },
   methods: {
-    getTree() {
-      organTreeByCurrUser().then(res => {
-        console.log(res)
-      })
-    },
     /**
-     *
-     * 点击节点回调函数
+     * 地图初始化事件
      */
-    handleNodeClick(data) {
-      console.log(data)
+    init(_map, _this) {
+      this.map = _map
+      this.page = _this
     },
 
     /**
-     *
+     * 获取数据
+     */
+    getTree() {
+      organTreeByCurrUser().then(res => {
+        if(res.code === 200) {
+          return res.data
+        } else {
+          throw new Error("organTreeByCurrUser()::::::数据错误")
+        }
+      }).then(data => {
+        this.config.treeData.option = data
+      })
+    },
+
+    /**
+     * 点击节点回调函数
+     */
+    handleNodeClick(data) {
+      // 调用地图组件中打点函数
+      this.page.addPoint(data)
+    },
+
+    /**
      * 选中级联选择器节点时触发
      */
     handleChange(value) {
