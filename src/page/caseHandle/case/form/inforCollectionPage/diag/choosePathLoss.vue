4afe9129ce35688b85f3fe0de31e09dcd1ef9990
wangcs1994 <2206613280@qq.com>
2020-08-18 18:22:37 +0800

    配补偿立案登记 添加路损清单 所属标准下拉框内容 修复

--- src/page/caseHandle/case/form/inforCollectionPage/diag/choosePathLoss.vue
+++ src/page/caseHandle/case/form/inforCollectionPage/diag/choosePathLoss.vue
@@ -17,7 +17,7 @@
           <div class="item">
             <el-form-item label="所属标准">
               <el-select v-model="pathLossSearchForm.roadLcBz" placeholder="请选择" @change="changeBz">
-                <el-option v-for="item in allRoadLcBz" :key="item" :label="item" :value="item"></el-option>
+                <el-option v-for="item in allRoadLcBz" :key="item.id" :label="item.name" :value="item.name"></el-option>
               </el-select>
             </el-form-item>
           </div>
@@ -171,14 +171,18 @@ export default {
     },
     //更改标准
     changeBz(val) {
+      let _this = this;    
       getDictListDetailByNameApi(val).then(
         res => {
-          this.allRoadType = res.data;
+          console.log("chopsepath",res.data);
+            _this.allRoadType=res.data;
+            _this.pathLossSearchForm.roadLcType="";                
         },
         err => {
           console.log(err);
         }
       );
+      
     },
     //是否禁止选择
     canSelectable(row, index) {
@@ -219,9 +223,18 @@ export default {
           }
         })
       })
+    },
+    //从数据字典获取路产标准
+    getRoadLcBz(){
+      let name = "公路路产所属标准";
+      getDictListDetailByNameApi(name).then(res=>{
+          this.allRoadLcBz=res.data;
+      })
     }
   },
-  mounted() {}
+  mounted() {
+    this.getRoadLcBz();
+  }
 };
 </script>
 <style lang="scss">
