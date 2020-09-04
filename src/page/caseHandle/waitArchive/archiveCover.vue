0beb88e0c7fb33a9bfb4e5e2d4193748d532a369
chenxiaoxiao <chenxiaoxiao@catsti.com>
2020-09-04 16:03:30 +0800

    修改免登录

--- src/page/caseHandle/waitArchive/archiveCover.vue
+++ src/page/caseHandle/waitArchive/archiveCover.vue
@@ -3,7 +3,8 @@
     <el-form ref="archiveCoverForm" :rules="rules" :model="formData" label-width="105px" v-show="showCover=='cover'">
       <div class="content_box">
         <div class="content">
-          <div class="content_title">青海公路路政执法案件</div>
+          <!-- <div class="content_title">青海公路路政执法案件</div> -->
+          <div class="content_title">{{formData.title}}</div>
           <div class="border_blue"></div>
           <div class="content_form">
             <el-form-item label="logo">
