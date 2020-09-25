0f177ee1a8027a6f5caee7723f25e5b68fa59360
qiangsf <qiangshaofeng@catsti.com>
2020-09-25 14:26:34 +0800

    feat(/trend.vue): 对接口

--- src/page/analyseManage/caseAnalysis/trend.vue
+++ src/page/analyseManage/caseAnalysis/trend.vue
@@ -4,13 +4,13 @@
       <div class="handlePart">
         <el-form :inline="true" :model="logForm" label-width="100px" ref="logForm">
           <el-form-item label="立案机构" prop>
-            <el-select size="small" v-model="state" placeholder="立案机构">
+            <el-select size="small" v-model="logForm.mechanism" placeholder="立案机构">
               <el-option label="全部" value></el-option>
 
             </el-select>
           </el-form-item>
           <el-form-item label="执法门类" prop>
-            <el-select size="small" v-model="state" placeholder="执法门类">
+            <el-select size="small" v-model="logForm.category" placeholder="执法门类">
               <el-option label="全部" value></el-option>
 
             </el-select>
@@ -21,11 +21,20 @@
             v-show="item.activeName === activeName"
             :key="index">
             <el-date-picker
+              size="small"
               v-model="item.value"
               :type="item.type"
               :placeholder="item.placeholder"
               :value-format="item.valueFormat">
             </el-date-picker>
+          </el-form-item>
+          <el-form-item>
+            <el-button
+              type="primary"
+              icon="el-icon-search"
+              size="small"
+              style="margin-left: 30px"
+              @click="handleSelect">搜索</el-button>
           </el-form-item>
         </el-form>
       </div>
@@ -36,7 +45,7 @@
           :key="item.name"
           :label="item.label"
           :name="item.name">
-            <div ref="echarts" style="width: 1000px; height: 400px;"></div>
+            <div :id="item.id" style="width: 1000px; height: 400px;"></div>
           </el-tab-pane>
         </el-tabs>
       </div>
@@ -55,44 +64,37 @@
         dateList: [
           {
             activeName: 'monthView',
-            value: "",
+            value: String(new Date().getFullYear()),
             type: "year",
             placeholder: "选择年",
             valueFormat: "yyyy"
           },
           {
             activeName: 'dayView',
-            value: "",
+            value: String(new Date().getFullYear()) + '/' + ((new Date().getMonth() + 1) > 9 ? String((new Date().getMonth() + 1)) : ('0'+String((new Date().getMonth() + 1)))),
             type: "month",
             placeholder: "选择月",
             valueFormat: "yyyy/MM"
           },
           {
             activeName: 'hoursView',
-            value: "",
+            value: String(new Date().getFullYear()) + '/' + ((new Date().getMonth() + 1) > 9 ? String((new Date().getMonth() + 1)) : ('0'+String((new Date().getMonth() + 1)))) + '/' + (new Date().getDate() > 9 ? String(new Date().getDate()) : ('0'+String(new Date().getDate()))),
             type: "date",
             placeholder: "选择日期",
             valueFormat: "yyyy/MM/dd"
           },
         ],
         tabPans: [
-          { label: "年视图", name: "year" },
-          { label: "月视图", name: "monthView" },
-          { label: "日视图", name: "dayView" },
-          { label: "时视图", name: "hoursView" },
+          { label: "年视图", name: "yearView", id: "yearChart" },
+          { label: "月视图", name: "monthView", id: "monthChart" },
+          { label: "日视图", name: "dayView", id: "dayChart" },
+          { label: "时视图", name: "hoursView", id: "hoursChart" },
         ],
-        activeName: 'year',
-
-        state: "",
+        activeName: 'yearView',
         checked: true,
         logForm: {
-          organ: "",
-          type: "",
-          operation: "",
-          username: "",
-          startTime: "",
-          endTime: "",
-          dateArray: ""
+          mechanism: "",
+          category: ""
         },
       };
     },
@@ -103,13 +105,43 @@
       /**
        * 初始化页面，显示年视图
        */
-      init() {
-        sjglfx().then(res => {
-          let data = res.data
-          this.setCharts(data)
-        }).catch(() => {
-          throw new Error("sjglfx::::::接口数据错误")
+      init(params) {
+        sjglfx(params).then(res => {
+          if(res.code === 200) {
+            return res.data
+          } else {
+            throw new Error("sjglfx::::接口数据错误")
+          }
+        }).then(data => {
+          let echartsData = []
+          Object.keys(data).map(key => {
+            console.log(data[key].length)
+            if(data[key].length > 0) {
+              echartsData.push(data[key][0])
+            }
+          })
+          this.setCharts(echartsData)
         })
+      },
+
+      /**
+       * 点击搜索
+       */
+      handleSelect() {
+        let valMap = new Map([
+          [ 'monthView', this.dateList[0].value ],
+          [ 'dayView', this.dateList[1].value ],
+          [ 'hoursView', this.dateList[2].value ],
+        ])
+        let mechanism = this.logForm.mechanism
+        let category = this.logForm.category
+        if(this.activeName === 'yearView') {
+          let params = mechanism + '/' + category
+          this.init(params)
+        } else {
+          let params = mechanism + '/' + category + '/' + valMap.get(this.activeName)
+          this.getData(params)
+        }
       },
 
       /**
@@ -121,48 +153,75 @@
           [ 'dayView', this.dateList[1].value ],
           [ 'hoursView', this.dateList[2].value ],
         ])
-        if(val === 'year') {
+        if(val === 'yearView') {
           this.init()
         } else {
-          this.getData(val, dateMap.get(val))
+          this.getData(dateMap.get(val))
         }
       },
 
       /**
        * 获取数据
        */
-      getData(val, params) {
+      getData(params) {
         let axiosMap = new Map([
-          [ 'monthView', sjglfxmonth(params) ],
-          [ 'dayView', sjglfxday(params) ],
-          [ 'hoursView', sjglfxhours(params) ],
+          [ 'monthView', sjglfxmonth ],
+          [ 'dayView', sjglfxday ],
+          [ 'hoursView', sjglfxhours ],
         ])
-        axiosMap.get(val)
+        axiosMap.get(this.activeName)(params)
           .then(res => {
-            let data = res.data
-            this.setCharts(data)
-          }).catch(() => {
-            throw new Error("sjglfx::::::接口数据错误")
-          })
+            if(res.code === 200) {
+              return res.data
+            } else {
+              throw new Error("getData::::接口数据错误")
+            }
+          }).then(data => { this.setCharts(data) })
       },
 
       /**
        * 给图表赋值
        */
       setCharts(data) {
-        let xAxis = [], series = [];
         console.log(data)
-        // this.drawCharts({ xAxis, series })
+        let xAxis = [], series = [];
+        let type = this.activeName
+        data.map(item => {
+          let years = item.years || '-',
+          month = item.month || '-',
+          day = item.day || '-',
+          hours = item.dhoursay || '-'
+
+          if(type === 'yearView') {
+            xAxis.push(years)
+          } else if (type === 'monthView') {
+            xAxis.push((years + '/' + month))
+          } else if (type === 'dayView') {
+            xAxis.push((years + '/' + month + '/' + day))
+          } else if (type === 'hoursView') {
+            xAxis.push(hours)
+          }
+          series.push(item.value)
+        })
+        this.drawCharts({ xAxis, series })
       },
 
       /**
        * 折线图数据格式
        */
       drawCharts({ xAxis, series }) {
-        let dom = this.$refs.echarts
+        let idMap = new Map([
+          [ 'yearView', 'yearChart' ],
+          [ 'monthView', 'monthChart' ],
+          [ 'dayView', 'dayChart' ],
+          [ 'hoursView', 'hoursChart' ],
+        ])
+        let chartRef = idMap.get(this.activeName)
+        let dom = document.getElementById(chartRef)
+        console.log(dom)
         if(dom) {
           let myChart = echarts.init(dom)
-          const option = {
+          let option = {
             title: {
               text: "年度案发数量分析",
               left: "center"
@@ -183,7 +242,7 @@
             },
             series: [
               {
-                data: data,
+                data: series,
                 type: "line"
               }
             ]
