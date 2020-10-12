a1cda3376528eae64ff1353e1a2bb6fd63f39985
yangzitong <12345678>
2020-10-12 10:11:35 +0800

    分析研判接口调试

---
+++ src/page/analyseManage/report/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-dialog
+    title="提示"
+    :visible.sync="dialogVisible"
+    width="30%"
+    :before-close="handleClose">
+    <span>案件人员专题报告 <a class=‘download‘ :href='downloadhttp' download=""  title="下载">下载</a></span>
+
+    <span slot="footer" class="dialog-footer">
+  </span>
+  </el-dialog>
+</template>
+
+<script>
+  export default {
+    name: "index",
+    data() {
+      return {
+        downloadhttp:'',
+        dialogVisible: true
+      }
+    },
+    methods:{
+      download(){
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
