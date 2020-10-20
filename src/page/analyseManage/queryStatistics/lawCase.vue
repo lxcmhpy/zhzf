7301c8f1e9700fe499c7852388e3aa629259d035
qiangsf <qiangshaofeng@catsti.com>
2020-10-20 10:58:30 +0800

    style(/lawcase.vue,/personequip.vue): 修改背景图片

--- src/page/analyseManage/queryStatistics/lawCase.vue
+++ src/page/analyseManage/queryStatistics/lawCase.vue
@@ -7,7 +7,7 @@
             <div class="dptitle_1 dptitle_font" @click="ajpage()">执法案件</div>
           </el-col>
           <el-col :span="4" class="dptitle_2" style="margin-left: 13px;">
-            {{ mapTitle }}
+            <!-- 宁夏执法数据分析研判系统 -->
           </el-col>
           <el-col :span="6" style="height:60px;margin-left: -505px;margin-top: 35px;">
             <div class="dptitle_3 dptitle_font" @click="zbpage()">人员装备</div>
@@ -145,7 +145,6 @@
   export default {
     data() {
       return {
-        mapTitle: '',
         vehicle: '',
         penalty: '',
         all: '',
@@ -271,7 +270,6 @@
             that.mapData = res.data.mapdata
             axios.get('/static/json/map/changeMap.json').then(res => {
               mapTypes[res.data.mapType](that.mapData,'案件数量')
-              this.mapTitle = res.data.mapTitle
             })
             that.trend()    //年度案发趋势
             that.caseStatus()   //案件状态
@@ -2164,7 +2162,7 @@
   }
 
   .main {
-    background-image: url("../../../../static/images/map/边框1.png");
+    background-image: url("../../../../static/images/map/baseBorder.png");
     background-size: 100% 100%;
   }
 
