d570865d146e732eb751c1007fae0723cf539824
zhangjiating <zhangjiating@catsti.com>
2020-10-29 10:19:05 +0800

    考试管理接口报错提示多次修改

--- src/page/trained/trained-manage/components/addExam.vue
+++ src/page/trained/trained-manage/components/addExam.vue
@@ -132,7 +132,7 @@ export default {
               },
               err => {
                 loading.close();
-                this.$message({ type: "error", message: err.msg || "" });
+                // this.$message({ type: "error", message: err.msg || "" });
               }
             );
         } else {
