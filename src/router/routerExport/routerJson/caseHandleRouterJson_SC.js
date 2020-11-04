37c4e2b19597a9a0cbaafbe92f7ca9c28eacbad8
wangyiming <wangyiming>
2020-11-04 14:21:39 +0800

    四川文书

--- src/router/routerExport/routerJson/caseHandleRouterJson_SC.js
+++ src/router/routerExport/routerJson/caseHandleRouterJson_SC.js
@@ -32,6 +32,38 @@ export default [
     },
     component: () => import("@/page/caseHandle/case/siChuan/doc/evidenceRegApprovalForm_SC")
     },
+    {
+      path: "/adminCoerciveMeasureApproval_SC",
+      name: "case_handle_adminCoerciveMeasureApproval_SC",
+      meta: {
+        title: "行政强制措施审批表-四川"
+      },
+      component: () => import("@/page/caseHandle/case/siChuan/doc/adminCoerciveMeasureApproval_SC.vue")
+    },
+    {
+      path: "/prolongAdminCoerciveMeasureApproval_SC",
+      name: "case_handle_prolongAdminCoerciveMeasureApproval_SC",
+      meta: {
+        title: "延长行政强制措施审批表-四川"
+      },
+      component: () => import("@/page/caseHandle/case/siChuan/doc/prolongAdminCoerciveMeasureApproval_SC.vue")
+    },
+    {
+      path: "/removeAdminCoerciveMeasureApproval_SC",
+      name: "case_handle_removeAdminCoerciveMeasureApproval_SC",
+      meta: {
+        title: "解除行政强制措施审批表-四川"
+      },
+      component: () => import("@/page/caseHandle/case/siChuan/doc/removeAdminCoerciveMeasureApproval_SC.vue")
+    },
+    {
+      path: "/enforceDecideApprovalForm_SC",
+      name: "case_handle_enforceDecideApprovalForm_SC",
+      meta: {
+        title: "行政强制执行审批表"
+      },
+      component: () => import("@/page/caseHandle/case/siChuan/doc/enforceDecideApprovalForm_SC.vue")
+    },
    
     
     
