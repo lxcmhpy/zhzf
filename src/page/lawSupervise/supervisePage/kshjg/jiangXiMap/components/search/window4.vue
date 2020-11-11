85d86da6d83c55936b8c5814a44f8b88fc6dfa24
qiangsf <qiangshaofeng@catsti.com>
2020-11-11 17:45:11 +0800

    feat(search/window4.vue): 添加轨迹图功能

--- src/page/lawSupervise/supervisePage/kshjg/jiangXiMap/components/search/window4.vue
+++ src/page/lawSupervise/supervisePage/kshjg/jiangXiMap/components/search/window4.vue
@@ -14,12 +14,51 @@
         </div>
       </div>
     </div>
+    <!-- 轨迹图选择框 -->
+    <div class="search-window4-selectTrack" v-if="showTrack">
+      <div class="header">
+        <span>选择模式</span>
+        <i class="el-icon-close" @click="clickTrackWindow"></i>
+      </div>
+      <div class="track" @click="handleShowTrack(1)">实时轨迹</div>
+      <div class="track" @click="handleShowTrack(2)">历史轨迹</div>
+      <!-- 实时轨迹输入框 -->
+      <div class="window" v-if="showTheTime">
+        <el-radio v-model="radio" label="1">当前位置</el-radio>
+        <el-radio v-model="radio" label="2">从本时间开始</el-radio>
+        <div class="theTimeInput">
+          <el-input class="input" v-model="theTime"></el-input>
+          <span class="text">分钟前</span>
+        </div>
+        <div class="submitBtns">
+          <el-button type="primary" plain @click="submitTrack(1)">确认</el-button>
+          <el-button plain @click="showTheTime=false">取消</el-button>
+        </div>
+      </div>
+      <!-- 历史轨迹输入框 -->
+      <div class="window" v-if="showHistory">
+        <el-date-picker
+          class="selectTime"
+          v-model="historyTime"
+          type="daterange"
+          range-separator="至"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+          value-format="yyyy-MM-dd">
+        </el-date-picker>
+        <div class="submitBtns">
+          <el-button type="primary" plain @click="submitTrack(2)">确认</el-button>
+          <el-button plain @click="showHistory=false">取消</el-button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { getGpsTraceList } from "@/api/lawSupervise.js"
 export default {
-  inject: ['page'],
+  inject: ['page', 'indexPage'],
   props: {
     window4: {
       type: Object,
@@ -28,12 +67,87 @@ export default {
       }
     }
   },
+  data() {
+    return {
+      showTrack: false,
+      showTheTime: false,
+      showHistory: false,
+      theTime: '',
+      historyTime: '',
+      radio: ''
+    }
+  },
   computed: {
     list() {
       return (this.window4 && this.window4.info) || []
     }
   },
   methods: {
+    /**
+     * 点击轨迹选项
+     */
+    handleShowTrack(val) {
+      console.log(val)
+      if(val === 1) {
+        this.showHistory = false
+        this.showTheTime = true
+        this.indexPage.page.removeFeatureById()
+      } else if (val === 2) {
+        this.showTheTime = false
+        this.showHistory = true
+        this.indexPage.page.removeFeatureById()
+      }
+    },
+
+    /**
+     * 提交轨迹表单
+     */
+    submitTrack(val) {
+      if(val === 1) { // 实时轨迹表单
+        this.showTheTime = false
+        let params = {
+          deviceId: this.window4.info.padSn,
+          deviceType: 0
+        }
+        let layerName = "ssgj"
+        this.getTrackPoints(params, layerName)
+      } else if (val === 2) { // 历史轨迹表单
+        this.showHistory = false
+        let params = {
+          startTime: this.historyTime[0],
+          endTime: this.historyTime[1]
+        }
+        let layerName = "lsgj"
+        this.getTrackPoints(params, layerName)
+      }
+    },
+
+    /**
+     * 获取轨迹数据
+     */
+    getTrackPoints(params, layerName) {
+      getGpsTraceList(params).then(res => {
+        if(res.code === 200) {
+          return res.data
+        } else {
+          throw new Error('getGpsTraceList:::::接口数据错误')
+        }
+      }).then(data => {
+        let points = data.map(item => {
+          return [item.x, item.y]
+        })
+        this.indexPage.page.addLine(points, this.window4.info.id, layerName)
+      })
+    },
+
+    /**
+     * 关闭轨迹框
+     */
+    clickTrackWindow() {
+      this.showTrack = false
+      this.indexPage.page.removeFeatureById()
+    },
+
     /**
      * 点击返回
      */
@@ -45,6 +159,9 @@ export default {
      * 点击底部小图标
      */
     handleClickBtns(index) {
+      if(index === 4) {
+        this.showTrack = true
+      }
       this.$emit('handleClickBtns', index, this.window4.info)
     }
   },
@@ -56,6 +173,90 @@ export default {
   border-radius: 4px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
   background: #FFFFFF;
+  position: relative;
+  &-selectTrack {
+    position: absolute;
+    width: 130px;
+    top: 0;
+    left: 380px;
+    .header {
+      background: #909399;
+      height: 46px;
+      text-align: center;
+      position: relative;
+      line-height: 46px;
+      span {
+        color: #FFFFFF;
+        font-size: 14px;
+        font-weight: 400;
+      }
+      .el-icon-close {
+        color: #FFFFFF;
+        position: absolute;
+        line-height: 46px;
+        right: 10px;
+        cursor: pointer;
+      }
+    }
+    .track {
+      background: #DCDFE6;
+      height: 46px;
+      line-height: 46px;
+      color:#606266;
+      font-size: 14px;
+      font-weight: 400;
+      text-align: center;
+      cursor: pointer;
+    }
+    .track:hover {
+      background: #409EFF;
+      color: #FFFFFF;
+    }
+    .window {
+      position: absolute;
+      height: 207px;
+      width: 300px;
+      border-radius: 4px;
+      top: 0;
+      left: 133px;
+      background: #FFFFFF;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+      box-sizing: border-box;
+      padding: 40px 10px 20px 10px;
+      .selectTime {
+        width: 100%;
+        .el-range-separator {
+          width: 8%;
+        }
+        .el-input__inner {
+          padding: unset;
+        }
+      }
+      .theTimeInput {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 20px;
+        .input {
+          flex: 2;
+        }
+        .text {
+          flex: 1;
+          color:#606266;
+          font-size: 14px;
+          font-weight: 400;
+          text-align: center;
+        }
+      }
+      .submitBtns {
+        position: absolute;
+        width: 100%;
+        bottom: 20px;
+        text-align: center;
+      }
+    }
+  }
   &-header {
     background: #00adb5;
     height: 46px;
