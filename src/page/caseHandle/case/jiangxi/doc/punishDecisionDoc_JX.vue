12159de7dcd2ecb9207ffef070715de3599ba47c
yanglei <yanglei@lecarling.com>
2020-11-11 16:05:41 +0800

    没有时分秒去年月日对比

--- src/page/caseHandle/case/jiangxi/doc/punishDecisionDoc_JX.vue
+++ src/page/caseHandle/case/jiangxi/doc/punishDecisionDoc_JX.vue
@@ -548,7 +548,7 @@ export default {
   methods: {
     starttime(){
       console.log('案发时间=='+this.docData.lasj)
-      if (Date.parse(this.docData.makeDate) < Date.parse(this.docData.lasj)) {
+      if (Date.parse(this.docData.makeDate) < Date.parse(this.docData.lasj.substr(0,10))) {
         this.$message({
           message: '当前时间不得小于立案时间',
           type: 'warning'
