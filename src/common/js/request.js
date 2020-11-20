3cc9dee62b9de34cb6408632be5ed8c478a88158
qiangsf <qiangshaofeng@catsti.com>
2020-11-20 11:30:46 +0800

    fix(/window4.vue): 调整经纬度顺序

--- src/common/js/request.js
+++ src/common/js/request.js
@@ -100,9 +100,9 @@ service.interceptors.request.use(
         if (response.data.code == 200) {
           tryHideFullScreenLoading();
           // debugger
-          console.log('response.data',response)
+          // console.log('response.data',response)
           //解密
-           response.data.data = JSON.parse(decrypt(response.data.data))
+          //  response.data.data = JSON.parse(decrypt(response.data.data))
           return response.data;
         } else if (
           response.data.code == 400 || response.data.code == 500) {
