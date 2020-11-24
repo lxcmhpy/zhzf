60d6c78000960edd76f9e19cae83c1c56e61b315
fanpengzhe <fanpengzhe@catsti.com>
2020-11-24 10:39:45 +0800

    修改了几个bug

--- src/page/inspection/record-manage/writeRecord.vue
+++ src/page/inspection/record-manage/writeRecord.vue
@@ -181,7 +181,7 @@ import {
 import { upload, deleteFileByIdApi, uploadCommon } from "@/api/upload.js";
 import iLocalStroage from "@/common/js/localStroage";
 import { mapGetters } from "vuex";
-
+import {  validatePhone ,validateIDNumber} from "@/common/js/validator";
 import merge from "webpack-merge";
 export default {
   props: ["psMsg"],
@@ -985,8 +985,9 @@ export default {
           }
         }
         if (item.type == "文本型") {
+          // debugger
           item.type = "input";
-          this.rule.push({
+          let inputRule={
             type: "input",
             field: item.id || item.field, //id用于传值，field用于预览
             title: item.title,
@@ -1005,7 +1006,22 @@ export default {
                 trigger: "blur",
               },
             ],
-          });
+          }
+          if(item.title==='企业联系方式'||item.title==='联系方式'){
+            inputRule.validate=[{
+              required: item.required == "true" ? true : false,
+              validator:validatePhone,
+              trigger: "blur",
+            }]
+          }
+          if(item.title==='证件号码'){
+            inputRule.validate=[{
+              required: item.required == "true" ? true : false,
+              validator:validateIDNumber,
+              trigger: "blur",
+            }]
+          }
+          this.rule.push(inputRule);
         } else if (item.type == "抽屉型") {
           item.options.forEach((option) => {
             option.label = option.value;
